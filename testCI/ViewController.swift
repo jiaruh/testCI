1bd78b50ba873a5c5f248e7926c2158173230af8
huangjiaru <huangjiaru@37.com>
2019-03-02 14:38:37 +0800

    Initial Commit

---
+++ testCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testCI
+//
+//  Created by jiaruh on 2019/3/2.
+//  Copyright © 2019 web. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
